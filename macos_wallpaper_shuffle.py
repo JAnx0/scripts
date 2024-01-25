ac054ecdb57e1f8700ca1ab465a17e033f72f56d
j-schmied <jannik.schmied@pm.me>
2024-01-25 11:12:17 +0100

    Updated to class concept

--- macos_wallpaper_shuffle.py
+++ macos_wallpaper_shuffle.py
@@ -3,7 +3,7 @@
 """
 Improved wallpaper shuffle script for macOS.
 
-Author: Jannik Schmied, 2023
+Author: Jannik Schmied, 2024
 """
 import argparse
 import filetype
@@ -16,6 +16,75 @@ import time
 
 from appscript import app, mactypes
 from collections import defaultdict
+
+
+class Wallpaper:
+    def __init__(self, path, analysis=False, restore=False):
+        self.path = path
+        self.analysis = analysis
+        self.restore = restore
+        self.images = [image for image in [file for file in os.listdir(self.path) if not os.path.isdir(os.path.join(self.path, file))] if filetype.is_image(os.path.join(self.path, image))]
+        self.current_wallpaper = app('Finder').desktop_picture.get()
+
+        if self.restore:
+            print("[*] Saved initial wallpaper (-r specified)")
+            self.initial_wallpaper = self.current_wallpaper
+
+        self.total_wallpapers = len(self.images)
+
+        if self.total_wallpapers == 0:
+            notify("WallpaperShuffle", "Error: No images found in specified directory!")
+            raise ValueError("[!] No images found in specified directory!")
+
+        self.total_wallpapers_shuffled = 0
+        self.last_img_name_buf = 0
+
+        if self.analysis:
+            self.image_counts = defaultdict(int)
+
+    def next(self):
+        # Get random image from folder
+        self.current_wallpaper = random.choice(self.images)
+
+        # Set wallpaper
+        app('Finder').desktop_picture.set(mactypes.File(os.path.join(self.path, self.current_wallpaper)))
+        print(f"[i] Current wallpaper: {self.current_wallpaper.ljust(self.last_img_name_buf)}", end="\r", flush=True)
+
+        # Increment wallpaper counter and sleep for interval
+        self.last_img_name_buf = len(self.current_wallpaper)
+        self.total_wallpapers_shuffled += 1
+
+        if self.analysis:
+            self.image_counts[self.current_wallpaper] += 1
+
+    def update(self):
+        self.images = [image for image in [file for file in os.listdir(self.path) if not os.path.isdir(os.path.join(self.path, file))] if filetype.is_image(os.path.join(self.path, image))]
+        if self.total_wallpapers != len(self.images):
+            print(f"[*] Updated images (Total: {self.total_wallpapers} -> {len(self.images)})")
+            notify("WallpaperShuffle", f"Updated images (Total: {self.total_wallpapers} -> {len(self.images)})")
+            self.total_wallpapers = len(self.images)
+
+    def analyze(self):
+        mean_usage = sum(self.image_counts.values()) / len(self.image_counts)
+        std_deviation = (sum((x - mean_usage) ** 2 for x in self.image_counts.values()) / len(self.image_counts)) ** 0.5
+
+        print(f"[i] Mean Usage: {mean_usage}")
+        print(f"[i] Standard Deviation: {std_deviation}")
+
+        plt.bar(self.image_counts.keys(), self.image_counts.values())
+        plt.xlabel("Images")
+        plt.ylabel("Usage Count")
+        plt.title("Histogram of Image Usage")
+        plt.xticks(rotation=90)
+        plt.show()
+
+    def restore_wallpaper(self):
+        app('Finder').desktop_picture.set(self.initial_wallpaper)
+        print("[*] Restored initial wallpaper")
+
+    @property
+    def wallpaper_counter(self):
+        return self.total_wallpapers_shuffled
 
 
 def notify(title: str, text: str, icon_path: str = "./assets/macos_beta.icns"):
@@ -45,50 +114,19 @@ def main():
         notify("WallpaperShuffle", "Error: Path does not exist or is not a directory!")
         raise ValueError("[!] Path does not exist or is not a directory!")
 
-    if args.restore:
-        # Get current wallpaper
-        print("[*] Saving current wallpaper")
-        current_wallpaper = app('Finder').desktop_picture.get()
-
-    images: list = [image for image in [file for file in os.listdir(args.path) if not os.path.isdir(os.path.join(args.path, file))] if filetype.is_image(os.path.join(args.path, image))]
-    total_wallpapers = len(images)
-
-    if total_wallpapers == 0:
-        notify("WallpaperShuffle", "Error: No images found in specified directory!")
-        raise ValueError("[!] No images found in specified directory!")
-
-    wallpaper_counter: int = 0
-    img_name_buf: int = 0
-    img_counts = defaultdict(int)
+    wallpaper = Wallpaper(args.path, analysis=args.analyze, restore=args.restore)
 
     print("[*] Starting wallpaper shuffle. Press CTRL+C to stop.")
     notify("WallpaperShuffle", "Starting wallpaper shuffle. Press CTRL+C to stop.")
 
     while True:
         try:
-            # Get random image from folder
-            image = random.choice(images)
-
-            # Set wallpaper
-            app('Finder').desktop_picture.set(mactypes.File(os.path.join(args.path, image)))
-            print(f"[i] Current wallpaper: {image.ljust(img_name_buf)}", end="\r", flush=True)
-
-            # Increment wallpaper counter and sleep for interval
-            img_name_buf = len(image)
-            wallpaper_counter += 1
-            img_counts[image] += 1
-
-            # Update images folder
-            images = [image for image in [file for file in os.listdir(args.path) if not os.path.isdir(os.path.join(args.path, file))] if filetype.is_image(os.path.join(args.path, image))]
-            if total_wallpapers != len(images):
-                print(f"[i] Updated images (Total: {total_wallpapers} -> {len(images)})")
-                notify("WallpaperShuffle", f"Updated images (Total: {total_wallpapers} -> {len(images)})")
-                total_wallpapers = len(images)
-
+            wallpaper.next()
+            wallpaper.update()
             time.sleep(args.interval)
 
         except KeyboardInterrupt:
-            print(f"\n[i] Stopped. Shuffled {wallpaper_counter} wallpapers.")
+            print(f"\n[*] Stopped. Shuffled {wallpaper.wallpaper_counter} wallpapers.")
             break
 
         except Exception as e:
@@ -96,23 +134,10 @@ def main():
             continue
 
     if args.restore:
-        # Reset wallpaper
-        print("[*] Restoring initial wallpaper")
-        app('Finder').desktop_picture.set(current_wallpaper)
+        wallpaper.restore_wallpaper()
 
     if args.analyze:
-        mean_usage = sum(img_counts.values()) / len(img_counts)
-        std_deviation = (sum((x - mean_usage) ** 2 for x in img_counts.values()) / len(img_counts)) ** 0.5
-
-        print(f"[i] Mean Usage: {mean_usage}")
-        print(f"[i] Standard Deviation: {std_deviation}")
-
-        plt.bar(img_counts.keys(), img_counts.values())
-        plt.xlabel('Images')
-        plt.ylabel('Usage Count')
-        plt.title('Histogram of Image Usage')
-        plt.xticks(rotation=90)
-        plt.show()
+        wallpaper.analyze()
 
     print("[*] Done. Exiting.")
     sys.exit(0)
